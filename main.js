96b8542db52a62f0f21f0f13a3872fd1c8280de3
michelegelmini <michelegelminivr@gmail.com>
2024-05-08 16:38:49 +0200

    add: math-logic

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ console.log('hello!');
 const km = parseInt(Number(prompt('Quanti km vuoi viaggiare?')));
 const age = parseInt(Number(prompt('Quanti anni hai?')));
 
+let price = km * 0.21;
+
 /* check inserimento valori */
 if (!(isNaN(km) || isNaN(age))) {
 
-    console.log('hai inserito due numeri');
+    if (age <= 18) {
+        console.log('sconto 20%');
+        let discountedPrice = price - (price * 20 / 100);
+        console.log(discountedPrice);
+    } else if (age >= 65) {
+        console.log('sconto 40%');
+        let discountedPrice = price - (price * 40 / 100);
+        console.log(discountedPrice);
+    } else {
+        console.log('niente sconto');
+        console.log(price);
+    }
 
 } else {
     console.log('inserisci due numeri');
