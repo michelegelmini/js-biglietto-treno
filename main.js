d3192d31e7e2d83612a2a9a3940ac4a4d8d2d6c4
michelegelmini <michelegelminivr@gmail.com>
2024-05-08 16:31:36 +0200

    add: value-check

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 console.log('hello!');
 
-const km = prompt('Quanti km vuoi viaggiare?');
-const age = prompt('Quanti anni hai?');
+const km = parseInt(Number(prompt('Quanti km vuoi viaggiare?')));
+const age = parseInt(Number(prompt('Quanti anni hai?')));
 
+/* check inserimento valori */
+if (!(isNaN(km) || isNaN(age))) {
+
+    console.log('hai inserito due numeri');
+
+} else {
+    console.log('inserisci due numeri');
+}
