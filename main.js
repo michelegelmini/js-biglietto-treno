f30bb3b405b121c4ede7f9a161e5002959a60a18
michelegelmini <michelegelminivr@gmail.com>
2024-05-08 17:47:01 +0200

    add: visual-imrovements

--- main.js
+++ main.js
@@ -4,35 +4,48 @@ const km = parseInt(Number(prompt('Quanti km vuoi viaggiare?')));
 const age = parseInt(Number(prompt('Quanti anni hai?')));
 
 let price = (km * 0.21).toFixed(2);
-let ticketOutput = document.getElementById('ticket-output');
+let priceOutput = document.getElementById('price');
+let discountOutput = document.getElementById('discount');
+let discountedPriceOutput = document.getElementById('discounted-price');
+let totalPriceOutput = document.getElementById('total-price');
+let messageWithDiscount = document.getElementById('messageWithDiscount');
 let discount = 0;
-let message = 'Ecco il prezzo del tuo biglietto'
 
 /* check inserimento valori */
 if (!(isNaN(km) || isNaN(age))) {
 
+
+
     if (age <= 18) {
         console.log('sconto 20%');
         discount = 20;
-        message = 'Congratulazioni! Sei minorenne quindi hai diritto ad uno sconto del 20%!'
+        messageWithDiscount = `Congratulazioni! Sei minorenne quindi hai diritto ad uno sconto del 20%!`
+
 
     } else if (age >= 65) {
         console.log('sconto 40%');
         discount = 40;
-        message = 'Congratulazioni! Sei vecchio quindi hai diritto ad uno sconto del 40%!'
+        messageWithDiscount = `Congratulazioni! Sei vecchio quindi hai diritto ad uno sconto del 40%!`
+
 
     } else {
         console.log('niente sconto');
         console.log(price);
+        discount = 0;
+        messageWithDiscount = ''
     }
 
     let discountedPrice = (price - (price * discount / 100)).toFixed(2);
 
-    ticketOutput.innerHTML =
-        `${message} <br>
-        Prezzo del biglietto: €${price} <br>
-        Sconto ${discount}%: €${(price * 20 / 100).toFixed(2)} <br>
-        Prezzo Scontato: €${discountedPrice}`
+    messageWithDiscount.innerHTML = `${messageWithDiscount}`
+    priceOutput.innerHTML = `€${price}`
+    discountOutput.innerHTML = `${discount}`
+    discountedPriceOutput.innerHTML = `€${(price * discount / 100).toFixed(2)}`
+    totalPriceOutput.innerHTML = `€${discountedPrice}`
+
+
+
+
 
 } else {
     console.log('inserisci due numeri');
